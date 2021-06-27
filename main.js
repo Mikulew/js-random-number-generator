78e7f8fe1f9e6049a26815641776e407aab661d6
Mikulew <mikulew@gmail.com>
2021-06-27 16:11:26 +0200

    Fix edge cases

--- main.js
+++ main.js
@@ -2,24 +2,36 @@ const output = document.querySelector('output');
 const button = document.querySelector('button');
 const inputMinimum = document.getElementById('minInput');
 const inputMaximum = document.getElementById('maxInput');
-const error = document.getElementById('error');
+const error = document.getElementById('errorText');
 
 function submit() {
-  const minValue = Number.parseInt(inputMinimum.value);
-  const maxValue = Number.parseInt(inputMaximum.value);
-  const isValid = minValue < maxValue;
-
-  return isValid ? generateRandomNumber(minValue, maxValue) : generateError();
+  const minValue = Number.parseFloat(inputMinimum.value);
+  const maxValue = Number.parseFloat(inputMaximum.value);
+  if (validateNumbers(minValue, maxValue)) {
+    return generateRandomNumber(minValue, maxValue);
+  }
 }
 
 function generateRandomNumber(min, max) {
-  error.classList.add('hidden');
-  output.value = Math.round(Math.random() * (max - min) + min);
+  return output.value = Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-function generateError() {
+function generateError(text) {
   output.value = '---';
-  error.classList.remove('hidden');
+  error.textContent = text;
+}
+
+function validateNumbers(min, max) {
+  error.textContent = '';
+  if (!(Number.isInteger(min) && Number.isInteger(max))) {
+    generateError("Numbers must be integers.");
+    return false;
+  }
+  if (min >= max) {
+    generateError("Minimum number must be less than maximum number.");
+    return false;
+  }
+  return true;
 }
 
 button.addEventListener('click', () => submit());
